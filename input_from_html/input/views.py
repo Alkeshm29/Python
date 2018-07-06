fa5639471b47f40e1a89f8940c767d56559d2178
Alkesh Mahajan <alkeshm29@gmail.com>
2018-07-06 14:19:19 +0530

    Update views.py

--- input_from_html/input/views.py
+++ input_from_html/input/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html",{})
+
+def disp(request):
+    request.method == 'POST'
+    name = request.POST.get('name', None)
+    print("Name",name)
+
+    return render(request,"disp.html",{'name':name})
