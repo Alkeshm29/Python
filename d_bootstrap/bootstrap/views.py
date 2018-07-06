7d57e51827f770d32a7697b0cb8e987828331f9f
Alkesh Mahajan <alkeshm29@gmail.com>
2018-07-06 23:39:44 +0530

    Bootstrap
    Simple Bootstrap implantation

---
+++ d_bootstrap/bootstrap/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"home.html",{})
+
+def disp(request):
+    # a = request()
+    request.method == 'POST'
+    name = request.POST.get('name', None)
+    print("Name",name)
+
+    return render(request,"disp.html",{'name':name})
