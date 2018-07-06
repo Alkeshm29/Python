1641ca10b35e8ebeac131a9fcf23c88216cd735b
Alkesh Mahajan <alkeshm29@gmail.com>
2018-07-06 14:18:40 +0530

    Update urls.py

--- input_from_html/input/urls.py
+++ input_from_html/input/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name = "home")
+    path('',views.home, name = "home"),
+    path('disp',views.disp, name ="disp"),
+
 ]
